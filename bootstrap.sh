d4d2d026b015dd9f63303a887104adc3ad5ae276
JesueJunior <jesuesousa@gmail.com>
2016-01-19 22:16:12 -0500

    improving bootstrap and VIM fonts

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,19 @@ ln -snf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -snf ~/dotfiles/vim ~/.vim
 ln -snf ~/dotfiles/vim/vimrc ~/.vimrc
 
-#ln -snf ~/Dropbox/git/ssh/config ~/.ssh/config
+ln -snf ~/Dropbox/git/ssh/config ~/.ssh/config
 
 ln -snf  ~/dotfiles/git/.gitconfig ~/.gitconfig
 
+if [ `uname` = "Darwin" ]
+then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install python3
+    brew install vim --with-python --with-ruby --with-perl
+    brew install macvim --env-std --override-system-vim
+    sudo easy_install pip
+fi
+
+sudo pip install mkvirtualenvwrapper
 vim +PluginInstall +qall
+
