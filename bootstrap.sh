6e0aa826f618e3c4715945a977e75db5b4ef2613
Jesue Junior <jesuesousa@gmail.com>
2016-04-11 22:11:24 -0300

    Improviments to centos

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,13 +36,26 @@ fi
 
 if [ `uname -o` = "GNU/Linux" ]
 then
-    sudo yum install -y cmake python-devel epel-release zsh git python-pip 
+    sudo yum install -y cmake python-devel epel-release zsh git python-pip yum-utils  
     sudo yum groupinstall "Development Tools" -y
+    sudo yum-builddep python
+    curl -O https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
+    tar xf Python-3.5.0.tgz
+    cd Python-3.5.0
+    ./configure
+    make
+    sudo make install
+    cd ..
+    rm -rf Python-3.5.0*
 fi
 
 sudo pip install virtualenvwrapper
 # Install fonts pretty good
 cd ~/dotfiles/fonts && bash install.sh
 vim +PluginInstall +qall
+
+#Fixing youcompleteme plugin
+cd .vim/bundle/YouCompleteMe/
+./install.py
 
 chsh -s /bin/zsh
